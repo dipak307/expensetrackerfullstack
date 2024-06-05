babd7fca60f5e4dcac773c083744280a1de876eb
dipak307 <114248721+dipak307@users.noreply.github.com>
2024-06-06 01:21:37 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,11 @@ const {readdirSync}=require('fs')
 const PORT=process.env.PORT;
 ///middleware
 app.use(express.json())
-app.use(cors())
+ app.use(cors({
+       origin:"http://localhost:3001",
+       methods:['POST','GET'],
+       credentials:true,
+     }))
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1' , require('./routes/' + route)))
