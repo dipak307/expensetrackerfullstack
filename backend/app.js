5a0bf4fb3de0c2dddd029b6a971ae820711ada2b
dipak307 <114248721+dipak307@users.noreply.github.com>
2024-06-06 15:43:03 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,11 @@ const {readdirSync}=require('fs')
 const PORT=process.env.PORT;
 ///middleware
 app.use(express.json())
- app.use(cors())
+app.use(cors({
+        origin:"https://expensetrackerfullstack-ol1o.vercel.app",
+        methods:['POST','GET'],
+        credentials:true,
+      }))
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1' , require('./routes/' + route)))
