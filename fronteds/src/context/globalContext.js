04b16dfe1a5b185e6d8988f66fd8e756b275f35c
dipak307 <114248721+dipak307@users.noreply.github.com>
2024-06-06 21:32:34 +0530

    Update globalContext.js

--- fronteds/src/context/globalContext.js
+++ fronteds/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 
-const BASE_URL="https://expensetrackerfullstack.vercel.app/api/v1/";
+const BASE_URL="https://expensetrackerfullstack-backend.vercel.app/api/v1/";
 
 const GlobalContext=React.createContext();
 
